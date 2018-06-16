035cb7307b879cbe22c597f0611e11350adfa2da
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-06-16 00:42:02 -0300

    add styled-components and remove unused code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const URL =
+  "https://api.github.com/search/repositories?q=more+react+facebook+vue";
+
+const Button = styled.button`
+  font-family: inherit;
+  font-size: inherit;
+  padding: 8px;
+  margin: 0;
+  color: white;
+  background-color: white-blue;
+  border: 0;
+  border-radius: 4px;
+  appearance: none;
+  &:hover {
+    background-color: black;
+  }
+`;
 
 class App extends Component {
+  state = {
+    react: 0,
+    vue: 0
+  };
+
+  getGithubStars = async () => {
+    const response = await fetch(URL);
+    const result = await response.json();
+
+    console.log(result);
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Button onClick={this.getGithubStars} />
       </div>
     );
   }
